a533bd040f8130c023ed742d42c98fc32fcdcf48
zakaria <grarizakaria@gmail.com>
2024-05-14 22:47:10 +0100

    task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" operations with pymongo"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ upadate topic in documents"""
+    filter_query = {"name": name}
+    update_query = {"$set": {"topics": topics}}
+    mongo_collection.update_one(filter_query, update_query)
